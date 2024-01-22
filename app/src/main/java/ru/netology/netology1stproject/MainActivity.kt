855b82af0d3c4376a958a3953e567cd9a8c162ee
Denis Aksenov <BLIHOK@mail.ru>
2024-01-22 19:30:20 +0300

    add images, menu, title, data, text, icons and counter

--- app/src/main/java/ru/netology/netology1stproject/MainActivity.kt
+++ app/src/main/java/ru/netology/netology1stproject/MainActivity.kt
@@ -2,10 +2,14 @@ package ru.netology.netology1stproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textTitleSize: TextView = findViewById(R.id.author)
+        textTitleSize.setOnClickListener { textTitleSize.isSingleLine = false }
     }
 }
